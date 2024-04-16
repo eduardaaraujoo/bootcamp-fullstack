63858377a7dc17e048a7faa115ef803e71ba6255
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-04-16 20:21:15 -0300

    Object

--- js/exercicio2.js
+++ js/exercicio2.js
@@ -40,11 +40,39 @@
 // var nomes = ["Thaiane", "Eduarda", "Bárbara"];
 
 //2. crie um array de pelo menos 5 elementos e add um item a ele.
-var nomes = ["Thaiane", "Juliana", "Maria", "Bárbara", "Eduarda"];
-nomes.push("Luke");
-console.log(nomes);
+// var nomes = ["Thaiane", "Juliana", "Maria", "Bárbara", "Eduarda"];
+// nomes.push("Luke");
+// console.log(nomes);
 
-//3. crie elemento de 5 elementos e remove um item.
-var nomes = ["Thaiane", "Juliana", "Maria", "Bárbara", "Eduarda"];
-nomes.pop();
-console.log(nomes);
+// //3. crie elemento de 5 elementos e remove um item.
+// var nomes = ["Thaiane", "Juliana", "Maria", "Bárbara", "Eduarda"];
+// nomes.pop();
+// console.log(nomes);
+
+
+//Objetos
+//São estruturas de dados específicas que podem englobar strings, numeros, booleanos, etc.
+//Para usar um objeto, usa-se {} => Chave e o valor
+// var aluno = {
+//     nome: "Eduarda",
+//     media: 10
+// }
+
+// console.log(aluno);
+// console.log(aluno.media);
+// console.log(aluno["nome"]);
+// console.log(aluno.nome);
+
+
+//Exercício
+//Criar uma função que recebe um array e retorne se o aluno foi aprovado
+function verificarAprovacao(nota) {
+    var media = (nota[0] + nota[1] + nota[2]) /3
+    
+    if(media >= 7){
+        return"Aprovado!"
+    }else{
+        return "Reprovado!"
+    }
+}
+console.log(calcularNota([6,8,2]));
