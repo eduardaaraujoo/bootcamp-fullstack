2c0fa4d2218d40743c94748f4beebc2732c13d83
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-04-19 08:59:18 -0300

    Modified exercicio3.js

--- js/exercicio3.js
+++ js/exercicio3.js
@@ -3,24 +3,28 @@
 //A lista deve ter no mínimo 2 itens
 //O valor total deve ser exibido com pelo menos 2 casa decimais
 
-const listaMercado = [
-    {nome: "Danone", preco: 3.50, quantidade: 6 },
-    {nome: "Coca", preco: 8.80, quantidade: 3 },
-    {nome: "Pão", preco: 9.00, quantidade: 9},
-    {nome: "Requeijão", preco: 7.00, quantidade: 2}
+var listaMercado = [
+    {nome: "Danone", preco: 3.50, quantidade: 6 },     //indice 0
+    {nome: "Coca", preco: 8.80, quantidade: 3 },       //indice 1
+    {nome: "Pão", preco: 9.00, quantidade: 9},         //indice 2
+    {nome: "Requeijão", preco: 7.00, quantidade: 2}    //indice 3 
 ]
 
-// Variável para armazenar o valor total da compra
-let valorTotal = 0;
+var contador = 0;
+var valorTotal = 0;
+
+while (contador < listaMercado.length){
+    valorTotal = listaMercado[contador].preco * listaMercado[contador].quantidade + valorTotal; //valorTotal está recebendo ele mesmo
+
+    contador++           // contador = contador + 1;
+    //contador ele acessa todos os índices da lista
+}
+console.log(valorTotal);
 
 
-// Exibindo a lista completa e calculando o valor total
-console.log("Lista de Compras:");
-listaMercado.forEach(item => {
-    const totalItem = item.preco * item.quantidade;
-    valorTotal = valorTotal + totalItem;
-    console.log(`Produto: ${item.nome} - Preço: R$${item.preco.toFixed(2)} - Quantidade: ${item.quantidade} - Total: R$${totalItem.toFixed(2)}`);
-});
 
-// Exibindo o valor total da compra
-console.log(`\nValor Total da Compra: R$${valorTotal.toFixed(2)}`);
+
+
+
+
+
