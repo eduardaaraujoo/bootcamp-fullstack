b90f75ab3ae25a253bb68c2ff45b7392842619a5
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-04-23 20:31:17 -0300

    Modified some file

--- js/exercicio5.js
+++ js/exercicio5.js
@@ -1,12 +1,17 @@
-//Laços de repetição
-var contador = 1;
-//while
-while(contador < 11){
-    console.log(contador);
-    contador++;
+//Orientado a objetos (POO)
+ 
+var aluno = { nome: "Caio", notas: [7,8,9] }
+var aluno2 = { nome: "Eduarda", notas: [9,6,9] }
+
+function calcularMedia(notas) {
+    var contador = 0;
+    var soma = 0;
+    while(contador < notas.length){
+        soma += notas[contador];
+    }
+
+    return soma / notas.length; 
 }
 
-//for -> for(inicialização do contador; condição ; incremento){}
-for(var contador2 = 0; contador2 < 11; contador2++){
-    console.log(contador2);
-}
+console.log(calcularMedia(aluno.notas));
+
