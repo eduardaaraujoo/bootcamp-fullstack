01c0bdda9ec9b47730e6cc8b941aa2a157826051
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-04-23 18:37:25 -0300

    Create the file  exercicio4.js

---
+++ js/exercicio5.js
@@ -0,0 +1,12 @@
+//Laços de repetição
+var contador = 1;
+//while
+while(contador < 11){
+    console.log(contador);
+    contador++;
+}
+
+//for -> for(inicialização do contador; condição ; incremento){}
+for(var contador2 = 0; contador2 < 11; contador2++){
+    console.log(contador2);
+}
