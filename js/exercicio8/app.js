0a28952718a84f9d5e4d2dab43410c72add2425b
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-05-03 12:12:05 -0300

    JSON

--- js/exercicio8/app.js
+++ js/exercicio8/app.js
@@ -6,3 +6,9 @@ const URL = "https://9554-2804-1b0-f189-3f57-9364-62f2-223d-e765.ngrok-free.app"
 //1. Consumir a rota livros que retorna um array de livros
 // Toda consulkta a uma API envolve uma requisição e uma resposta
 fetch(URL + "/livros")
+    .then((response) => {
+        return response.json()
+    })
+    .then((responseJSON) => {
+        console.log(responseJSON)
+    })
