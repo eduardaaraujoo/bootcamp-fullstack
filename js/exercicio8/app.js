63360fbacc47b86922113afcea6c5efe37af8ffe
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-05-03 09:04:42 -0300

    Create the folder exercicio8

---
+++ js/exercicio8/app.js
@@ -0,0 +1,8 @@
+
+
+const URL = "https://9554-2804-1b0-f189-3f57-9364-62f2-223d-e765.ngrok-free.app" //rota taiz da API
+
+
+//1. Consumir a rota livros que retorna um array de livros
+// Toda consulkta a uma API envolve uma requisição e uma resposta
+fetch(URL + "/livros")
