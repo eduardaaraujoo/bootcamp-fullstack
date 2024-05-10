ce252164e4683279dc08c4e0d772c53b6512b94f
eduardaaraujoo <dudsaraujo16@gmail.com>
2024-05-10 09:10:00 -0300

    Create js/exercicio9

---
+++ js/exercicio9/api.js
@@ -0,0 +1,104 @@
+var usuarios = [];
+let books = [
+    { title: "O Senhor dos Anéis", author: "J.R.R. Tolkien" },
+    { title: "Fundação", author: "Isaac Asimov" },
+    { title: "Duna", author: "Frank Herbert" }
+];
+
+// Rota que retorna um array de livros
+app.get('/livros', (req, res) => {
+
+    res.json(books);
+});
+
+// Rota que adiciona um livro ao array de livros { title: 'kdjskd', author: 'skldjsl'}
+app.post('/livros', (request, response) => {
+    const novoLivro = request.body
+    books.push(novoLivro);
+    response.json(books)
+});
+
+let location = {
+    latitude: -23.550520,
+    longitude: -46.633308
+};
+
+// Rota que retorna uma localização específica
+app.get('/localizacao', (req, res) => {
+    res.json(location);
+});
+
+// Rota que recebe a nova localização { latitude: askj, longitude: kjahsd }
+app.post('/localizacao', (request, response) => {
+    // Alterar a localização atual
+    const { latitude, longitude } = request.body
+    // const latitude = request.body.latitude
+    // const longitude = request.body.longitude
+
+    location.latitude = latitude
+    location.longitude = longitude
+
+    response.json(location)
+
+});
+
+// Rota de login 
+app.post('/login', (req, res) => {
+
+    const { email, password } = req.body; // desestruturação => { email: "teste@gmail.com", password: "123"}
+    // const email = req.body.email;
+    // const password = req.body.password;
+
+    const usuario = usuarios.find(u => u.email === email && u.password === password);
+    if (usuario) {
+        res.json(usuario);
+    } else {
+        res.status(400).json({ erro: "Usuário ou senha incorretos" });
+    }
+});
+
+// Rota de criação de usuários 
+app.post('/criar_usuario', (req, res) => {
+    const { email, password } = req.body;
+    if (usuarios.find(u => u.email === email)) {
+        res.status(400).json({ erro: "Email já está em uso" });
+    } else {
+        usuarios.push({ id: usuarios.length + 1, email, password });
+        res.json({ status: 'Usuário criado com sucesso' });
+    }
+});
+
+// Rota de editar de usuários 
+app.put('/editar_usuario/:id', (req, res) => {
+    const { id } = req.params;
+    const { email, password } = req.body;
+    let usuarioEncontrado = usuarios.find((u, index) => {
+        if (u.id === id) {
+            usuarios[index] = { email, password };
+            return true;
+        }
+    });
+
+    if (usuarioEncontrado) {
+        res.json({ status: 'Usuário atualizado com sucesso' });
+    } else {
+        res.status(404).json({ erro: 'Usuário não encontrado' });
+    }
+});
+
+// Rota de exclusão de usuários
+app.delete('/excluir_usuario/:id', (req, res) => {
+    const { id } = req.params;
+    const usuarioIndex = usuarios.findIndex(u => u.id === id);
+    if (usuarioIndex > -1) {
+        usuarios.splice(usuarioIndex, 1);
+        res.json({ status: 'Usuário excluído com sucesso' });
+    } else {
+        res.status(404).json({ erro: 'Usuário não encontrado' });
+    }
+});
+
+// Inicia o servidor na porta especificada
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
---
+++ js/exercicio9/app.js
@@ -0,0 +1,185 @@
+// const axios = require('axios');
+import axios from "axios";
+
+/*
+    Aula 12
+
+    Tópicos: 
+
+    1. Consumo de APIs com fetch
+    2. Consumo de APIs com axios
+
+*/
+
+//JSDoc: sintaxe para documentação simples em comentários dentro do próprio código
+
+// /**
+//  * Realiza a subtração de dois números de forma assíncrona.
+//  * @param {number} num1 - O primeiro número a ser subtraído.
+//  * @param {number} num2 - O segundo número a ser subtraído.
+//  * @returns {Promise<number>} Uma Promise que resolve com o resultado da subtração num1 - num2.
+//  * @throws {Promise<string>} Uma Promise que rejeita com uma mensagem de erro se algum dos parâmetros não for um número.
+//  */
+// const subtrairAssincrono = (num1, num2) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (typeof num1 !== "number" || typeof num2 !== "number") {
+//                 reject("Erro: Algum dos parâmetros não é um número")
+//             } else {
+//                 resolve(num1 - num2)
+//             }
+//         }, 3000)
+//     })
+// }
+
+const URL = "http://localhost:3000"
+
+// 1. Consumir a rota /livros que retorna um array de livros
+// Toda consulta a uma API envolve uma requisição e uma resposta
+// REQ -> RES
+
+// fetch(URL + "/livros")
+//     .then((response) => {
+//         // console.log(response) // Não está formatada em JSON - JavaScriptObjectNotation
+//         // Formatar, passar, converter para JSON
+//         return response.json()
+//     })
+//     .then((responseJSON) => {
+//         console.log(responseJSON)
+//     })
+
+// fetch(URL + "/livros")
+//     .then(resposta => {
+//         return resposta.json()
+//     })
+//     .then(respostaJson => {
+//         respostaJson.forEach((elemento) => {
+//             // 1. { title: "O Senhor dos Anéis", author: "J.R.R. Tolkien"}
+//             console.log(elemento.title + " - " + elemento.author)
+//         })
+//     })
+
+// 2. Buscar localização 
+// fetch(URL + "/localizacao")
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((responseJSON) => {
+//         console.log(responseJSON)
+//     })
+
+// 3. Atualizar localização a cada 1 segundo
+// setTimeout => executa algo APÓS X milisegundos
+// setInterval => executa algo de X em X milisegundos
+// setInterval(() => {
+//   fetch(URL + "/localizacao", {
+//     method: "GET"
+//   })
+//     .then((resposta) => resposta.json())
+//     .then((respostaJSON) => {
+//       console.log("Latitude:", respostaJSON.latitude);
+//       console.log("Longitude:", respostaJSON.longitude);
+//     });
+// }, 1000);
+
+// MÉTODOS/VERBOS HTTP
+/**
+ * GET -> LER/CONSULTAR DADOS
+ * POST -> ENVIAR DADOS/NOVOS DADOS
+ * PUT -> EDITAR DADOS JÁ CADASTRADOS
+ * DELETE -> EXCLUIR DADOS JÁ CADASTRADOS
+ */
+
+// fetch(URL + "/localizacao", {
+//   method: "GET"
+// })
+//   .then((resposta) => resposta.json())
+//   .then((respostaJSON) => {
+//     console.log("Latitude:", respostaJSON.latitude);
+//     console.log("Longitude:", respostaJSON.longitude);
+//   });
+
+// fetch(URL + "/localizacao", {
+//   method: "POST",
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({ latitude: -22.55052, longitude: -45.633308 })
+// })
+//   .then((resposta) => resposta.json())
+//   .then((respostaJSON) => {
+//     console.log("Latitude:", respostaJSON.latitude);
+//     console.log("Longitude:", respostaJSON.longitude);
+//   });
+
+// Exercício 1: Criar uma requisição POST para adicionar um livro { title, author }
+
+// const novoLivro = {
+//   title: "Programação em Java",
+//   author: "Deiel"
+// };
+
+// fetch(URL + "/livros", {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify(novoLivro)
+// })
+//   .then((retornoDoSucesso) => retornoDoSucesso.json())
+//   .then((responseJSON) => {
+//     console.log(responseJSON)
+//   })
+// .catch() retorno do erro
+// .finally() retorno depois do sucesso e do erro
+
+// Exercício 2: Reescrever o código da requisição anterior utilizando async await 
+
+// 1. Criar uma função assíncrona (async function)
+// 2. Escrever o bloco trycatch
+
+// async function requisicao(){
+//   await fetch();
+// }
+
+// const requisicaoAssincrona = async () => {
+//   try {
+//     let retornoDoSucesso = await fetch(URL + "/livros", {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(novoLivro)
+//     })
+//     let responseJSON = await retornoDoSucesso.json()
+//     console.log(responseJSON)
+//   }
+//   catch (erro) {
+//     console.log(erro)
+//   }
+// }
+
+// requisicaoAssincrona();
+
+// Utilizando a biblioteca AXIOS -> Realizar requisições HTTP
+// 1. Instalar a biblioteca: npm instal axios
+// 2. Escrever o campo "type" no arquivo package.json e colocar o valor "module" (para que o import funcione)
+// 3. Importar a biblioteca no arquivo: import axios from "axios"
+
+const novoLivro = {
+  title: "Programação em Java 2",
+  author: "Deiel 2"
+};
+
+const requisicaoAssincrona = async () => {
+  try {
+    //                   axios.metodo(url_da_api_com_a_rota, corpoDaRequisicao)
+    let response = await axios.post(URL + "/livros", novoLivro);
+    console.log(response.data)
+  }
+  catch (erro) {
+    console.log(erro)
+  }
+}
+
+requisicaoAssincrona();
